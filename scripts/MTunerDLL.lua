5abeb4354411f9dd0a5d36b8afd4fd13b6524845
Milos Tosic <milos@rudji.com>
2026-03-26 07:56:09 +0100

    Move to zidar build system

---
+++ scripts/MTunerDLL.lua
@@ -0,0 +1,12 @@
+--
+-- Copyright 2025 Milos Tosic. All rights reserved.   
+-- License: http://www.opensource.org/licenses/BSD-2-Clause
+--
+
+function projectDependencies_MTunerDLL()
+	return { "rmem", "rbase" }
+end
+
+function projectAdd_MTunerDLL() 
+	addProject_lib("MTunerDLL", Lib.Tool, true)
+end
