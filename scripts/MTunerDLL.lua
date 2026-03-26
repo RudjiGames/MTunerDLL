353509bdc4b83f6e68ce37e42be1c965a21a7554
Milos Tosic <milos@rudji.com>
2026-03-26 10:38:25 +0100

    Fix ci after move to zidar

--- scripts/MTunerDLL.lua
+++ scripts/MTunerDLL.lua
@@ -7,6 +7,10 @@ function projectDependencies_MTunerDLL()
 	return { "rmem", "rbase" }
 end
 
+function projectExtraConfig_MTunerDLL()
+	includedirs { path.join(projectGetPath("MTunerDLL"), "src/") }
+end
+
 function projectAdd_MTunerDLL() 
 	addProject_lib("MTunerDLL", LibraryType.Tool, true)
 end
